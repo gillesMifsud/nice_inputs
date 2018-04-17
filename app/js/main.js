399aa7e87bc14cfbf79dd4294c0ce022b781b4ba
Gilles MIFSUD <gilles.mifsud@netapsys.fr>
2018-04-17 08:36:24 +0200

    Nice checkboxes and radios material design

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $('form input[type="text"], form input[type="password"]').on('focus', function () {
+    /*niceInputs.on('focus', function () {
         var elem = $( this );
         var parentElem = elem.closest('div');
         if(elem.is(":focus")) {
@@ -8,10 +8,31 @@ $(document).ready(function(){
         }
     });
 
-    $('form input[type="text"], form input[type="password"]').on('blur', function () {
+    niceInputs.on('blur', function () {
         var elem = $( this );
         var parentElem = elem.closest('div');
         parentElem.removeClass("focused");
+    });*/
+    //  Au focus
+    var niceInputs = $('[data-niceInput] input');
+    niceInputs.focus(function(){
+        $(this).parent().addClass('is-focused has-label');
+    });
+
+    // à la perte du focus
+    niceInputs.blur(function(){
+        $parent = $(this).parent();
+        if($(this).val() == ''){
+            $parent.removeClass('has-label');
+        }
+        $parent.removeClass('is-focused');
+    });
+
+    // si un champs est rempli on rajoute directement la class has-label
+    niceInputs.each(function(){
+        if($(this).val() != ''){
+            $(this).parent().addClass('has-label');
+        }
     });
 
 });
